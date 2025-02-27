c6adf8a6640e43f0ca710cdb27c0e0cf37cc1fbe
VProPlayer <vkchaudhari2007@gmail.com>
2025-02-27 18:10:24 -0500

    base changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -88,10 +88,15 @@ public final class Constants {
       // new Translation2d(-wheelBase / 2.0, trackWidth / 2.0), // front left (-,+) back left
       // new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0) // back left (-,-) back right
       // fix rotaion, battery side currently, rio side actual front 
-      new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0), // front right (+,+) front left
-      new Translation2d(wheelBase / 2.0, trackWidth / 2.0), // back right (+,-) front right
-      new Translation2d(wheelBase / 2.0, -trackWidth / 2.0), // front left (-,+) back left
-      new Translation2d(-wheelBase / 2.0, trackWidth / 2.0) // back left (-,-) back right
+      new Translation2d(-wheelBase / 2.0, trackWidth / 2.0), // front left (-,+)
+      new Translation2d(wheelBase / 2.0, trackWidth / 2.0), // front right (+,+)
+      new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0), // back left (-,-)
+      new Translation2d(wheelBase / 2.0, -trackWidth / 2.0) // back right (+,-)
+
+      // new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0), // front right (+,+) front left
+      // new Translation2d(wheelBase / 2.0, trackWidth / 2.0), // back right (+,-) front right
+      // new Translation2d(wheelBase / 2.0, -trackWidth / 2.0), // front left (-,+) back left
+      // new Translation2d(-wheelBase / 2.0, trackWidth / 2.0) // back left (-,-) back right
 
 
       
